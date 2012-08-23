4be2dc0f2c589cc18b31514154aba035bced03be
Curtis Wendel <cwendel16834@gmail.com>
2012-08-23 08:09:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reunion::Application.config.secret_token = 'be59a3a176beb837e61bcf9c90d3b03bdf477e278c33887cd66f5100d538ab26b92dcdcf264e46e47f9c2246f4144c975220c407c3031b187f72bfb2ebd744a9'
